531e6b6415b7dc88e857b8450dbe5502b55310c3
marco <marco@marco-Q143F>
2014-12-10 17:47:02 +0100

    Server: collection<BookmarkResource>

--- BookmarkExample/src/controller/BookmarkController.java
+++ BookmarkExample/src/controller/BookmarkController.java
@@ -1,6 +1,16 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.springframework.hateoas.ResourceSupport;
+import org.springframework.hateoas.Resources;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
+import org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +24,23 @@ import resourcemodel.*;
 @RestController
 @RequestMapping("/{username}/bookmarks")
 public class BookmarkController {
+	
+    @RequestMapping(method = RequestMethod.GET)
+    public Collection<BookmarkResource> readBookmarks(@PathVariable String username) {
+
+        //this.validateUser(username);
+
+    	List<BookmarkResource> bookmarkResourceList = new LinkedList<BookmarkResource>();
+    	
+    	Map<Long, Bookmark> bookmarks = DBSingleton.getIstance().getAccounts().get(username).getBookmarks();
+    	for (Long id : bookmarks.keySet()) {  		
+    		bookmarkResourceList.add(new BookmarkResource(bookmarks.get(id)));
+		}
+ 
+    	return (Collection<BookmarkResource>)bookmarkResourceList;
+        //return new Resources<BookmarkResource>(bookmarkResourceList);
+
+    }
 	
 	@RequestMapping(value = "/{bookmarkId}", method = RequestMethod.GET)
 	public BookmarkResource readBookmark(@PathVariable String username, @PathVariable Long bookmarkId) {
