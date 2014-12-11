a838a044d67ada867fc82c7c6c3b86872895eb25
Lorenzo Di Maio <lolly.dimaio@gmail.com>
2014-12-11 15:52:21 +0100

    prova conflitto

--- BookmarkExample/src/controller/SimpleCORSFilterController.java
+++ BookmarkExample/src/controller/SimpleCORSFilterController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SimpleCORSFilterController implements Filter {
+	//commento di lorenzo
 
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) res;
